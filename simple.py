f8d65ffc8c7cf4ef21b5a63e5d0b31bc257b2bef
DocsGuy <111306113+DocsGuy@users.noreply.github.com>
2022-09-10 11:52:26 +0200

    Update simple.py

--- simple.py
+++ simple.py
@@ -5,10 +5,10 @@ import time
 #-------------------------------------------------------------------------------------------#
 N = 4
 String = ""
-def randomString():
+def bot():
     for i in range(90):
         String = ''.join(random.choices(string.ascii_lowercase + string.digits, k=N))
         os.system(f"start https://www.bing.com/search?q={String}")
         time.sleep(0.4)
         os.system("echo Hi")
-randomString()
+bot()
